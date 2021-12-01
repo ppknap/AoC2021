7dfd540cdaed629026e54ddfaf98fa40caeab180
Pawel Knap <pawelknap88@gmail.com>
2021-12-01 13:53:06 +0100

    chore: add solutions for AoC day one

--- Day01.jl
+++ Day01.jl
@@ -1,2 +1,9 @@
+data = map(x->parse(Int64, x), readlines("Day01.csv"));
+
 # Part 1
-println("Hello world")
+ans = count(map(x->(x[1] > 1 && x[2] - data[x[1]-1] > 0), enumerate(data)));
+println("Part 1: $ans")
+
+# Part 2
+ans = count(map(x->(x[1] > 3 && x[2] - data[x[1]-3] > 0), enumerate(data)));
+println("Part 2: $ans")
